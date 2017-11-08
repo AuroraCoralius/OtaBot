f79704ab7cfc18100059b258b2222d09fd7b41c1
Tenrys <tenrys.iaido@live.fr>
2017-11-08 08:16:30 +0100

    Add !resetcolor, !seecolor, !help

--- bot.lua
+++ bot.lua
@@ -13,12 +13,12 @@ client:on("ready", function()
 end)
 
 local hex2rgb = function(hex)
-    hex = hex:gsub("#", "")
-    if hex:len() == 3 then
-        return tonumber("0x" .. hex:sub(1, 1)) * 17, tonumber("0x" .. hex:sub(2, 2)) * 17, tonumber("0x" .. hex:sub(3, 3)) * 17
-    elseif hex:len() == 6 then
-        return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
-    end
+	hex = hex:gsub("#", "")
+	if hex:len() == 3 then
+		return tonumber("0x" .. hex:sub(1, 1)) * 17, tonumber("0x" .. hex:sub(2, 2)) * 17, tonumber("0x" .. hex:sub(3, 3)) * 17
+	elseif hex:len() == 6 then
+		return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
+	end
 end
 local hex2num = function(hex)
 	hex = hex:gsub("#", "")
@@ -29,8 +29,38 @@ local hex2num = function(hex)
 	end
 end
 
+local function errorChat(channel, msg)
+	channel:send({
+		embed = {
+			description = msg,
+			color = 0xFF4040
+		}
+	})
+end
 local cmdPrefix = "^[%.!/]"
 local commands = {
+	seecolor = {
+		callback = function(msg, args)
+			local guild = msg.guild
+			local botMember = guild.members:get(client.user.id)
+			local authorMember = guild.members:get(msg.author.id)
+
+			local arg = args[1] and args[1]:gsub("#", ""):upper() or nil
+			if not arg then errorChat(msg.channel, "Invalid color! Hex format only.") return end
+
+			local color = arg and hex2num(arg) or nil
+			if color then
+				-- Announce success!
+				msg.channel:send({
+					embed = {
+						description = ":arrow_left: This is what `" .. arg .. "` looks like.",
+						color = color
+					}
+				})
+			end
+		end,
+		help = "Preview a color! Accepts colors in Hex format (ex. #FF0000 = red)."
+	},
 	color = {
 		callback = function(msg, args)
 			local guild = msg.guild
@@ -40,7 +70,7 @@ local commands = {
 			-- Do we have permissions to fuck with roles?
 			if botMember:hasPermission(enums.permission.manageRoles) then
 				local arg = args[1] and args[1]:gsub("#", ""):upper() or nil
-				if not arg then return end
+				if not arg then errorChat(msg.channel, "Invalid color! Hex format only.") return end
 
 				local color = arg and hex2num(arg) or nil
 				if color then
@@ -64,7 +94,7 @@ local commands = {
 						role = guild:createRole("#" .. arg)
 						-- local roleColor = Color(color) -- unnecessary
 						role:setColor(color)
-						role:moveUp()
+						role:moveUp() -- show above Tenno role
 					end
 
 					-- Set role.
@@ -79,8 +109,60 @@ local commands = {
 					})
 				end
 			end
-		end
-	}
+		end,
+		help = "Change your color! Accepts colors in Hex format (ex. #FF0000 = red)."
+	},
+	resetcolor = {
+		callback = function(msg)
+			local guild = msg.guild
+			local botMember = guild.members:get(client.user.id)
+			local authorMember = guild.members:get(msg.author.id)
+
+			-- Do we have permissions to fuck with roles?
+			if botMember:hasPermission(enums.permission.manageRoles) then
+				-- Remove other color roles you had...
+				for role in authorMember.roles:iter() do
+					if role.name:match("^#") then
+						authorMember:removeRole(role.id)
+					end
+				end
+
+				-- Announce success!
+				msg.channel:send({
+					embed = {
+						description = authorMember.fullname .. "'s color has been reset.",
+						color = 0x7FFF40
+					}
+				})
+			end
+		end,
+		help = "Reset your color."
+	},
+	help = {
+		callback = function(msg)
+			local guild = msg.guild
+			local botMember = guild.members:get(client.user.id)
+			local authorMember = guild.members:get(msg.author.id)
+
+			local _msg = {
+				embed = {
+					title = "Available commands:",
+					fields = {},
+					color = 0x9B65BD
+				}
+			}
+			for cmd, cmdData in next, commands do
+				local fields = _msg.embed.fields
+				fields[#fields + 1] = {
+					name = cmd,
+					value = cmdData.help
+				}
+			end
+
+			msg.channel:send(_msg)
+		end,
+		help = "Displays this."
+	},
 }
 client:on("messageCreate", function(msg)
 	local text = msg.content
