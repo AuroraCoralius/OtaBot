56baade1c1b3d94dd1b628c305ae3e96d4fe996c
Tenrys <tenrys.iaido@live.fr>
2017-11-22 23:32:13 +0100

    nice i didnt commit this

--- bot.lua
+++ bot.lua
@@ -25,6 +25,9 @@ json = require("json")
 xml = require("./libs/xml.lua").newParser()
 base64 = require("base64")
 
+childprocess = require("childprocess")
+process = require("process")
+
 local _, magick = pcall(require, "magick")
 _G.magick = magick
 
