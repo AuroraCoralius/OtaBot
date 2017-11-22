0041b65176c6fdd4dc6ebfb404adbd8a5501a97b
Tenrys <tenrys.iaido@live.fr>
2017-11-22 23:59:12 +0100

    testing 1 2 3

--- commands.lua
+++ commands.lua
@@ -121,6 +121,7 @@ local function call(callback, msg, args, line)
 		end)()
 	end, msg, args, line)
 end
+print("this ran")
 client:on("messageCreate", function(msg)
 	local text = msg.content
 	local prefix = text:match(cmdPrefix)
