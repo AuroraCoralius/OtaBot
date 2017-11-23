7c0bf220a4fc0ea5169d1e4b387a13ca791ccb92
Tenrys <tenrys.iaido@live.fr>
2017-11-23 01:37:26 +0100

    Add config example

---
+++ config_example.lua
@@ -0,0 +1,9 @@
+return {
+	token = "", -- Discord bot token - https://discordapp.com/developers/applications/me
+	yandex_api = "", -- Yandex Translate API key - https://tech.yandex.com/translate/
+	owners = {
+		["138685670448168960"] = true, -- Tenrys
+		-- add your own Discord user ID below with the same format as above
+	},
+	mal = "" -- My Anime List credentials encoded in Base64 - https://myanimelist.net/register.php
+}
