c2258791d33158db08990e8c751a66a1463121f5
Tenrys <tenrys.iaido@live.fr>
2017-11-25 14:30:14 +0100

    the fuck

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ while true; do
 	echo -e "\e[42;30m START \e[0m"
 	echo
 	luvit bot.lua
+	$exit=$?
 	if [ -f "restart" ]; then
 		echo
 		echo -e "\e[46;30m RESTART \e[0m"
@@ -16,10 +17,9 @@ while true; do
 		echo -e "\e[43;30m STOP \e[0m"
 		echo
 	fi
-	if [ $? != "0" ]; then
-		echo -e "\e[41;30m ERROR \e[0m Bot didn't exit cleanly, code: \e[30;46m$?\e[0m"
+	if [ $exit != "0" ]; then
+		echo -e "\e[41;30m ERROR \e[0m Bot didn't exit cleanly, code: \e[30;46m $exit \e[0m"
 	fi
-	touch exit_code
-	echo -n $? > exit_code
+	echo -n $exit > exit_code
 done
 
