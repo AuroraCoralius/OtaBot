40aadbcc9a806eaea72abadae28d07dcb347c9d7
Tenrys <tenrys.iaido@live.fr>
2017-12-06 21:15:34 +0100

    autistic

--- libs/table_extension.lua
+++ libs/table_extension.lua
@@ -28,8 +28,7 @@ local valueTypeEnclosure = {
 local function table_tostring(tbl, depth, inline)
 	local str = "{" .. (inline and "" or "\n")
 	curDepth = curDepth + 1
-	local ourDepth = curDepth
-	print(depth, curDepth, ourDepth)
+	print(depth, curDepth)
 	local numOnly = true
 	for k in next, tbl do
 		if not isnumber(k) then
@@ -39,15 +38,15 @@ local function table_tostring(tbl, depth, inline)
 	end
 	for k, v in next, tbl do
 		local lastK = k == table.getlastkey(tbl)
-		str = str .. ("\t"):rep(inline and 0 or ourDepth) .. (numOnly and "" or "[" .. (isstring(k) and '"%s"' or "%s"):format(istable(k) and table_tostring(k, 1, true) or tostring(k)) .. "] = ")
-		if type(v) == "table" and ourDepth ~= depth then
-			str = str .. table_tostring(v, depth - ourDepth)
+		str = str .. ("\t"):rep(inline and 0 or curDepth) .. (numOnly and "" or "[" .. (isstring(k) and '"%s"' or "%s"):format(istable(k) and table_tostring(k, 1, true) or tostring(k)) .. "] = ")
+		if type(v) == "table" and curDepth > depth then
+			str = str .. table_tostring(v, depth - 1)
 		else
 			str = str .. (valueTypeEnclosure[type(v)] or "%s"):format(tostring(v))
 		end
 		str = str .. (lastK and "" or "," .. (inline and " " or "\n"))
 	end
-	str = str .. (inline and "" or "\n") .. ("\t"):rep(inline and 0 or ourDepth - 1) .. "}"
+	str = str .. (inline and "" or "\n") .. ("\t"):rep(inline and 0 or curDepth - 1) .. "}"
 	return str
 end
 function table.tostring(tbl, depth)
