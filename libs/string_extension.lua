836867e9a825f523f08dc10953474265e0ce0a70
Tenrys <tenrys.iaido@live.fr>
2017-12-13 03:14:47 +0100

    convert to tabs

--- libs/string_extension.lua
+++ libs/string_extension.lua
@@ -33,11 +33,11 @@ end
 
 function string.urlencode(str)
 	if str then
-    	str = str:gsub("\n", "\r\n")
-    	str = str:gsub("([^%w ])",
-    		function(c) return string.format("%%%02X", string.byte(c)) end
-    	)
-    	str = str:gsub(" ", "+")
+		str = str:gsub("\n", "\r\n")
+		str = str:gsub("([^%w ])",
+			function(c) return string.format("%%%02X", string.byte(c)) end
+		)
+		str = str:gsub(" ", "+")
 	end
 	return str
 end
