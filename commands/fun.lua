34b2b9102a9a9f6b3ab4aee598cefcdec3849796
Tenrys <tenrys.iaido@live.fr>
2018-01-28 15:21:09 +0100

    update: meme

---
+++ commands/fun.lua
@@ -0,0 +1,33 @@
+-- 『 』
+-- ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ
+-- ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ
+
+local commands = bot.commands
+
+local charMap = {}
+for i = 97, 122 do
+	charMap[i] = string.char(i)
+end
+for i = 65, 90 do
+	charMap[i] = string.char(i)
+end
+
+commands.help = {
+	callback = function(msg, line, cmd)
+		local str = "『"
+
+		for _, char in next, line:split("") do
+			local anime = charMap[char:byte()]
+			str = str .. (anime or char)
+		end
+
+		str = str .. "』"
+
+		msg.channel:send(str)
+	end,
+	help = {
+		text = "Convert text to something ressembling a Stand name from JoJo.",
+	},
+	category = "Fun"
+}
+
